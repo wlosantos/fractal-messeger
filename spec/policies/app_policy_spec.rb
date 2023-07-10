613ec7a2a8f5a6f6a96c453c7ebc1a45f1c0f152
wendellopes <wendelopes@msn.com>
2023-07-10 10:18:37 -0300

    create auth documentation swagger

--- spec/policies/app_policy_spec.rb
+++ spec/policies/app_policy_spec.rb
@@ -1,4 +1,6 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe AppPolicy, type: :policy do
   let!(:admin) { create(:user, :admin) }
@@ -7,51 +9,51 @@ RSpec.describe AppPolicy, type: :policy do
   subject { described_class }
 
   permissions :index? do
-    it "denies access if user is not admin" do
+    it 'denies access if user is not admin' do
       expect(subject).not_to permit(user, app)
     end
 
-    it "grants access if user is admin" do
+    it 'grants access if user is admin' do
       expect(subject).to permit(admin, app)
     end
   end
 
   permissions :show? do
-    it "denies access if user is not admin" do
+    it 'denies access if user is not admin' do
       expect(subject).not_to permit(user, app)
     end
 
-    it "grants access if user is admin" do
+    it 'grants access if user is admin' do
       expect(subject).to permit(admin, app)
     end
   end
 
   permissions :create? do
-    it "denies access if user is not admin" do
+    it 'denies access if user is not admin' do
       expect(subject).not_to permit(user, app)
     end
 
-    it "grants access if user is admin" do
+    it 'grants access if user is admin' do
       expect(subject).to permit(admin, app)
     end
   end
 
   permissions :update? do
-    it "denies access if user is not admin" do
+    it 'denies access if user is not admin' do
       expect(subject).not_to permit(user, app)
     end
 
-    it "grants access if user is admin" do
+    it 'grants access if user is admin' do
       expect(subject).to permit(admin, app)
     end
   end
 
   permissions :destroy? do
-    it "denies access if user is not admin" do
+    it 'denies access if user is not admin' do
       expect(subject).not_to permit(user, app)
     end
 
-    it "grants access if user is admin" do
+    it 'grants access if user is admin' do
       expect(subject).to permit(admin, app)
     end
   end
